368f546fa3918baa47f2d22efa0ab26ebf77768b
Giuseppe Grilli <giugrilli@gmail.com>
2016-12-08 19:42:48 +0100

    decaffeinating more

--- spec/config/config-gmaps-auto.js
+++ spec/config/config-gmaps-auto.js
@@ -1,4 +1,9 @@
-$(document).ready(function() { 
-  GmapsAutoComplete.init();
-  GmapsAutoComplete.autoCompleteInit();
+jQuery(function() {
+    let completer = new GmapsCompleter({
+        inputField: '#gmaps-input-address',
+        errorField: '#gmaps-error'
+    });
+
+    return completer.autoCompleteInit({
+        country: "us"});
 });
